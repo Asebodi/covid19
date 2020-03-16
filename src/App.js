209c374b227910d9e1862ec632258c47a1f1947e
Asebodi <aditraharditya45@gmail.com>
2020-03-17 00:51:27 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./css/style.css";
+
+function useStats() {
+  const [stats, setStats] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      console.log("Fetching data");
+      const data = await fetch(
+        "https://covid19.mathdro.id/api/countries/ID"
+      ).then(res => res.json());
+      setStats(data);
+    }
+    fetchData();
+  }, []);
+  return stats;
+}
+
+function Stats() {
+  const stats = useStats();
+  if (!stats) return <p className="loading">Loading....</p>;
+  console.log(stats);
+  const dateString = new Date(stats.lastUpdate);
+
+  return (
+    <div>
+      <main>
+        <div className="stats-wrapper infected">
+          <p>Confirmed:</p>
+          <h4 className="infected-val">{stats.confirmed.value}</h4>
+        </div>
+
+        <div className="stats-wrapper recovered">
+          <p>Recovered:</p>
+          <h4 className="recovered-val">{stats.recovered.value}</h4>
+        </div>
+
+        <div className="stats-wrapper deaths">
+          <p>Deaths:</p>
+          <h4 className="deaths-val">{stats.deaths.value}</h4>
+        </div>
+      </main>
+
+      <footer>
+        <p>Last updated: {dateString.toString()}</p>
+        <small>raharditya.com</small>
+      </footer>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="page-wrapper">
+        <header>
+          <h3>COVID-19 Monitor</h3>
+          <h4>Region: Indonesia</h4>
+        </header>
+
+        <Stats></Stats>
+      </div>
+    </body>
   );
 }
 
