f82af5b30efe7eecdb7c96002da9973398419476
Asebodi <aditraharditya45@gmail.com>
2020-03-17 15:20:37 +0700

    Added global data

--- src/App.js
+++ src/App.js
@@ -16,33 +16,99 @@ function useStats() {
   return stats;
 }
 
+function useStatsGlobal() {
+  const [statsGlobal, setStatsGlobal] = useState();
+  useEffect(() => {
+    async function fetchDataGlobal() {
+      console.log("Fetching data");
+      const data = await fetch("https://covid19.mathdro.id/api/").then(res =>
+        res.json()
+      );
+      setStatsGlobal(data);
+    }
+    fetchDataGlobal();
+  }, []);
+  return statsGlobal;
+}
+
+function Global() {
+  const statsGlobal = useStatsGlobal();
+  console.log(statsGlobal);
+  if (!statsGlobal) return <p className="loading">Loading....</p>;
+
+  return (
+    <div>
+      <h4 className="title">WORLDWIDE</h4>
+
+      <div className="global-wrapper">
+        <div className="global-stats">
+          <p>Confirmed</p>
+          <h4 className="infected-val">{statsGlobal.confirmed.value}</h4>
+        </div>
+
+        <div className="global-stats">
+          <p>Recovered</p>
+          <h4 className="recovered-val">{statsGlobal.recovered.value}</h4>
+        </div>
+
+        <div className="global-stats">
+          <p>Deaths</p>
+          <h4 className="deaths-val">{statsGlobal.deaths.value}</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function Stats() {
+  const format = require("date-format");
   const stats = useStats();
+  const statsGlobal = useStatsGlobal();
   if (!stats) return <p className="loading">Loading....</p>;
+  if (!statsGlobal) return <p className="loading">Loading....</p>;
   console.log(stats);
-  const dateString = new Date(stats.lastUpdate);
+  // const dateString = new Date(stats.lastUpdate);
+  // const dateStringGlobal = new Date(statsGlobal.lastUpdate);
 
   return (
     <div>
       <main>
-        <div className="stats-wrapper infected">
-          <p>Confirmed:</p>
-          <h4 className="infected-val">{stats.confirmed.value}</h4>
-        </div>
+        <Global></Global>
 
-        <div className="stats-wrapper recovered">
-          <p>Recovered:</p>
-          <h4 className="recovered-val">{stats.recovered.value}</h4>
-        </div>
+        <h4>INDONESIA</h4>
+        <div className="desktop-flex">
+          <div className="stats-wrapper infected">
+            <p>Confirmed:</p>
+            <h4 className="infected-val">{stats.confirmed.value}</h4>
+          </div>
 
-        <div className="stats-wrapper deaths">
-          <p>Deaths:</p>
-          <h4 className="deaths-val">{stats.deaths.value}</h4>
+          <div className="stats-wrapper recovered">
+            <p>Recovered:</p>
+            <h4 className="recovered-val">{stats.recovered.value}</h4>
+          </div>
+
+          <div className="stats-wrapper deaths">
+            <p>Deaths:</p>
+            <h4 className="deaths-val">{stats.deaths.value}</h4>
+          </div>
         </div>
       </main>
 
       <footer>
-        <p>Last updated: {dateString.toString()}</p>
+        <p>Last update</p>
+        <p>
+          Worldwide:{" "}
+          {format.asString(
+            "dd-MM-yyyy hh:mm:ss",
+            new Date(statsGlobal.lastUpdate)
+          )}{" "}
+          WIB
+        </p>
+        <p>
+          Indonesia:{" "}
+          {format.asString("dd-MM-yyyy hh:mm:ss", new Date(stats.lastUpdate))}{" "}
+          WIB
+        </p>
         <small>raharditya.com</small>
       </footer>
     </div>
@@ -55,7 +121,6 @@ function App() {
       <div className="page-wrapper">
         <header>
           <h3>COVID-19 Monitor</h3>
-          <h4>Region: Indonesia</h4>
         </header>
 
         <Stats></Stats>
